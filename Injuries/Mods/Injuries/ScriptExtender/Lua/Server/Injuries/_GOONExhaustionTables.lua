d631a036b857c29fd018accb1aa9933b36329a8c
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2025-02-19 09:53:22 +1100

    Fixed and overhauled sleeping mechanics for exhaustion

--- Injuries/Mods/Injuries/ScriptExtender/Lua/Server/Injuries/_GOONExhaustionTables.lua
+++ Injuries/Mods/Injuries/ScriptExtender/Lua/Server/Injuries/_GOONExhaustionTables.lua
@@ -9,19 +9,15 @@ local TechnicalStatusDurations = {
     GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_7 = {1, 10}
 }
 
--- Mapping of sleep statuses to their corresponding technical statuses
+-- Mapping of technical statuses to their corresponding non-technical statuses
 local TechnicalToSleepStatusMap = {
-    GOON_FALL_ASLEEP_EASY =
-    "GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_1",
-    "GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_2",
-    GOON_FALL_ASLEEP_MEDIUM =
-    "GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_3",
-    "GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_4",
-    GOON_FALL_ASLEEP_HARD =
-    "GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_5",
-    "GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_6",
-    GOON_FALL_ASLEEP_EXTREME =
-    "GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_7"
+    GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_1 = "GOON_FALL_ASLEEP_EASY",
+    GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_2 = "GOON_FALL_ASLEEP_EASY",
+    GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_3 = "GOON_FALL_ASLEEP_MEDIUM",
+    GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_4 = "GOON_FALL_ASLEEP_MEDIUM",
+    GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_5 = "GOON_FALL_ASLEEP_HARD",
+    GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_6 = "GOON_FALL_ASLEEP_HARD",
+    GOON_FALL_ASLEEP_CONSTITUTION_TECHNICAL_7 = "GOON_FALL_ASLEEP_EXTREME"
 }
 
 -- Function to apply a random duration sleep status
@@ -32,10 +28,10 @@ local function ApplySleepStatus(object, technicalStatus)
         if durationRange then
             -- Generate a random duration within the specified range and convert to seconds
             local sleepDuration = math.random(durationRange[1], durationRange[2]) * 6
-            -- Apply the sleep status with the random duration
-            Osi.ApplyStatus(object, sleepStatus, sleepDuration, 1)
             -- Apply the cooldown status with the same duration
             Osi.ApplyStatus(object, "GOON_FALL_ASLEEP_COOLDOWN", sleepDuration, 1)
+            -- Apply the sleep status with the random duration
+            Osi.ApplyStatus(object, sleepStatus, sleepDuration, 1)
         end
     end
 end
