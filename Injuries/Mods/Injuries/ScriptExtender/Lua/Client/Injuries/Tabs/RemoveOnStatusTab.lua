5b3b051b91a663257135612142cf869556342bb5
osirisofgit <github.com.j95fw@passmail.net>
2025-03-01 18:28:58 -0600

    more loca

--- Injuries/Mods/Injuries/ScriptExtender/Lua/Client/Injuries/Tabs/RemoveOnStatusTab.lua
+++ Injuries/Mods/Injuries/ScriptExtender/Lua/Client/Injuries/Tabs/RemoveOnStatusTab.lua
@@ -76,7 +76,7 @@ local function BuildRows(statusTable, status, injury, removeOnConfig, ignoreExis
 	end
 
 	if statusObj.Name == "LONG_REST" then
-		saveCell:AddText("After how many long rests? (Counted by event or status application, if no event triggers)")
+		saveCell:AddText(Translator:translate("After how many long rests? (Counted by event or status application (including Angelic Slumber), if no event triggers)"))
 		statusConfig["after_x_applications"] = statusConfig["after_x_applications"] or 1
 		saveCell:AddSliderInt("", statusConfig["after_x_applications"], 1, 30).OnChange = function (slider)
 			statusConfig["after_x_applications"] = slider.Value[1]
@@ -225,4 +225,5 @@ Translator:RegisterTranslation({
 	["Save Conditions"] = "hbe9455faa0784cb99616e5098fd5247dgcg2",
 	["# of Stacks To Remove (?)"] = "h125c3399732d41a4a300eee966450e16e61f",
 	["i.e. if you set 3rd Degree Burns to remove 2 stacks, you'll have 1st Degree Burns applied"] = "he6354d4907a7431b82c7ebde28275f9a7eae",
+	["After how many long rests? (Counted by event or status application (including Angelic Slumber), if no event triggers)"] = "h0334268fe1274b1e9eb4c0286c0321042ag7",
 })
